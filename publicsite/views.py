4dffafbcb0056ccb06d7fc0020f743bc22a5b37e
oxffaa <akulyakova.artem@gmail.com>
2016-04-10 19:52:54 +0700

    main page was implemented (draft)

--- publicsite/views.py
+++ publicsite/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
-
+from .models import Event
 
 def view_event(request, event_name=None):
-    return render(request, 'view_event.html', None)
+    event = None
+    if event_name:
+        event = Event.objects.get_by_name(event_name)
+    if not event:
+        event = Event.objects.get_current()
+    return render(request, 'view_event.html' if event else 'empty_event.html', event)
