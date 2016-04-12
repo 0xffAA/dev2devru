7835e69291a35404a13f990dd2253be55f56ab6d
0xffaa <akulyakov.artem@gmail.com>
2016-04-13 01:08:24 +0700

    base draft and dirty structure

--- publicsite/urls.py
+++ publicsite/urls.py
@@ -5,5 +5,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.current_event, name='main'),
-    url(r'^(?P<event_name>[\w\-_]*)$', views.view_event, name='event_name')
+    url(r'^(?P<event_name>[\w\-_]*)$', views.view_event, name='event_name'),
+    url(r'^history$', views.events_history, name='history'),
+    url(r'^about$', views.about, name='about'),
+    url(r'^contribute$', views.contribute, name='contribute')
 ]
--- publicsite/views.py
+++ publicsite/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render
 from .models import Event
 
+
 def current_event(request):
     event = Event.objects.get_current()
     return render(request, 'view_event.html' if event else 'empty_event.html', event)
 
 
-
 def view_event(request, event_name=None):
     event = Event.objects.get_by_name(event_name)
     return render(request, 'view_event.html', event)
+
+
+def events_history(request):
+    pass
+
+
+def about(request):
+    pass
+
+
+def contribute(request):
+    pass
