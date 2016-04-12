393f9643e7109605a6c7ddfead69df46d2d197c0
0xffaa <akulyakov.artem@gmail.com>
2016-04-13 00:40:13 +0700

    urlconf's was fixed more handlers

--- publicsite/urls.py
+++ publicsite/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 
 urlpatterns = [
-    url(r'$^', views.view_event, name='main'),
+    url(r'^$', views.current_event, name='main'),
+    url(r'^(?P<event_name>[\w\-_]*)$', views.view_event, name='event_name')
 ]
--- publicsite/views.py
+++ publicsite/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from .models import Event
 
-def view_event(request, event_name=None):
-    event = None
-    if event_name:
-        event = Event.objects.get_by_name(event_name)
-    if not event:
-        event = Event.objects.get_current()
+def current_event(request):
+    event = Event.objects.get_current()
     return render(request, 'view_event.html' if event else 'empty_event.html', event)
+
+
+
+def view_event(request, event_name=None):
+    event = Event.objects.get_by_name(event_name)
+    return render(request, 'view_event.html', event)
