facb65e9e6eb0eb8c590a54a9bbd903229cb8127
0xffaa <akulyakov.artem@gmail.com>
2016-04-15 04:06:27 +0700

    view models was added + small refactoring

--- publicsite/views.py
+++ publicsite/views.py
@@ -1,15 +1,28 @@
 from django.shortcuts import render
 from .models import Event
+from .view_models import EventVM
 
 
 def current_event(request):
-    event = Event.objects.get_current()
-    return render(request, 'view_event.html' if event else 'empty_event.html', event)
+    event = EventVM(
+        Event.objects.get_current()
+    )
+    return render(
+        request,
+        'view_event.html' if event else 'empty_event.html',
+        {'event': event}
+    )
 
 
 def view_event(request, event_name=None):
-    event = Event.objects.get_by_name(event_name)
-    return render(request, 'view_event.html', event)
+    event = EventVM(
+        Event.objects.get_by_name(event_name)
+    )
+    return render(
+        request,
+        'view_event.html',
+        {'event': event}
+    )
 
 
 def events_history(request):
