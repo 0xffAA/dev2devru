e60a40d1f599c9d5da556fe1fd084907cc83b508
0xffaa <akulyakov.artem@gmail.com>
2016-04-14 23:35:13 +0700

    sql logging was added

--- dev2devru/settings.py
+++ dev2devru/settings.py
@@ -123,3 +123,22 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Logging
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        }
+    },
+    'loggers': {
+        'django.db.backends': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+    }
+}
