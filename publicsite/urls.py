2f5ec5059e549f6cfead304e851dcac1048fb4bc
0xffaa <akulyakov.artem@gmail.com>
2016-04-18 00:07:55 +0700

    urls was fixed, /%event_name% -> /event/%event_name%

--- publicsite/urls.py
+++ publicsite/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.current_event, name='main'),
-    url(r'^(?P<event_name>[\w\-_]*)$', views.view_event, name='event_name'),
+    url(r'^event/(?P<event_name>[\w\-_]*)$', views.view_event, name='event_name'),
     url(r'^history$', views.events_history, name='history'),
     url(r'^about$', views.about, name='about'),
     url(r'^contribute$', views.contribute, name='contribute')
