6f0435aad5532207930fb869bec0d9e41f4354a5
0xffaa <akulyakov.artem@gmail.com>
2016-05-07 13:05:20 +0700

    watch task was added

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -4,7 +4,11 @@ var gulp = require('gulp')
 var sass = require('gulp-sass')
 
 gulp.task('sass', function () {
-    return gulp.src('./style/publicsite/*.sass')
+    return gulp.src('./style/publicsite/style.sass')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('../publicsite/static/'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch('./style/publicsite/*.sass', ['sass']);
 });
