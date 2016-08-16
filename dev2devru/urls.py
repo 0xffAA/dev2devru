249a51be51a2f82ef37c8e5516d334fec2436337
0xffaa <akulyakov.artem@gmail.com>
2016-08-16 17:59:17 +0700

    add surfing loaded images

--- dev2devru/urls.py
+++ dev2devru/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     url(r'^', include('publicsite.urls')),
-    url(r'^admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls)
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
