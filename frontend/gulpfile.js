34284a101c3c4e77f21192c9d6365734f17175db
0xffaa <akulyakov.artem@gmail.com>
2016-08-27 15:51:23 +0700

    bower integration was added, gulp refactoring

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -3,41 +3,76 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
+var bower = require('gulp-bower');
+var uglify = require('gulp-uglify');
+var bowerFiles = require('gulp-main-bower-files');
+var filter = require('gulp-filter');
 
-gulp.task('img', function () {
-    return gulp.src(['./img/publicsite/*', './img/*', '!./img/publicsite/'])
-        .pipe(gulp.dest('../publicsite/static/'));
+var paths = {
+    publicsite: {
+        input : {
+            app: './app/publicsite/*.js',
+            style: './style/publicsite/style.sass',
+            img: ['./img/publicsite/*', './img/*', '!./img/publicsite/'],
+            font: './font/*',
+        },
+        output : '../publicsite/static/'
+    },
+    libs: './libs/',
+}
+
+gulp.task('bower-update', function () {
+    return bower({ cmd: 'update' });
 });
 
-gulp.task('js', function () {
-    return gulp.src('./app/publicsite/*.js')
+gulp.task('libs-concat', ['bower-update'], function () {
+    return gulp.src('./bower.json')
+    .pipe(bowerFiles())
+    .pipe(filter('**/*.js', { restore: true }))
+    .pipe(concat('lib.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest(paths.libs))
+});
+
+gulp.task('img-publicsite', function () {
+    return gulp.src(paths.publicsite.input.img)
+        .pipe(gulp.dest(paths.publicsite.output));
+});
+
+gulp.task('app-publicsite', function () {
+    return gulp.src(paths.publicsite.input.app)
         .pipe(concat('app.js'))
-        .pipe(gulp.dest('../publicsite/static/'));
+        .pipe(gulp.dest(paths.publicsite.output));
 });
 
-gulp.task('sass', function () {
-    return gulp.src('./style/publicsite/style.sass')
+gulp.task('style-publicsite', function () {
+    return gulp.src(paths.publicsite.input.style)
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('../publicsite/static/'));
+        .pipe(gulp.dest(paths.publicsite.output));
 });
 
-gulp.task('font', function () {
-    return gulp.src('./font/*')
-        .pipe(gulp.dest('../publicsite/static/'));
+gulp.task('font-publicsite', function () {
+    return gulp.src(paths.publicsite.input.font)
+        .pipe(gulp.dest(paths.publicsite.output));
 });
 
-gulp.task('publicsite', function () {
-    return gulp.run('font', 'sass', 'img', 'js');
-});
+gulp.task(
+    'publicsite',
+    ['font-publicsite', 'style-publicsite', 'img-publicsite', 'app-publicsite'],
+    function () {
+        return gulp.src(paths.libs + '*')
+            .pipe(gulp.dest(paths.publicsite.output))
+    }
+);
 
-gulp.task('watch', function() {
+gulp.task('watch', ['publicsite'], function() {
     gulp.watch(
         [
-            './style/publicsite/*.sass',
-            './app/publicsite/*.js'
+            paths.publicsite.input.app,
+            paths.publicsite.input.style
         ],
         [
-            'sass',
-            'js'
+            'style-publicsite',
+            'app-publicsite'
         ]);
 });
