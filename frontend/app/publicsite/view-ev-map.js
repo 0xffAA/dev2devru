562cd5dee8dcc59b3dff9ed0d7171ef18ba76e01
0xffaa <akulyakov.artem@gmail.com>
2016-09-01 12:38:24 +0700

    refactoring

--- frontend/app/publicsite/view-ev-map.js
+++ frontend/app/publicsite/view-ev-map.js
@@ -1,70 +1,72 @@
-'use strict';
+;(function (ymaps) {
+    'use strict';
 
-ymaps.ready(function () {
-    var container = getContainer('view-ev-map-ycontainer');
-    var data = readData(container);
+    ymaps.ready(function () {
+        var container = getContainer('view-ev-map-ycontainer');
+        var data = readData(container);
 
-    var map = new ymaps.Map(container, data.config);
+        var map = new ymaps.Map(container, data.config);
 
-    var placemark = new ymaps.Placemark(
-        data.position,
-        {},
-        data.icon
-            ? {
-                iconLayout: 'default#image',
-                iconImageHref: data.icon,
-                iconImageSize: [30, 30],
-                iconImageOffset: [-15, -30],
+        var placemark = new ymaps.Placemark(
+            data.position,
+            {},
+            data.icon
+                ? {
+                    iconLayout: 'default#image',
+                    iconImageHref: data.icon,
+                    iconImageSize: [30, 30],
+                    iconImageOffset: [-15, -30],
+                }
+                : {
+                    preset: 'twirl#blackDotIcon',
+                }
+        );
+        map.geoObjects.add(placemark);
+
+        function getContainer(className) {
+            var elements = document.getElementsByClassName(className);
+
+            if (!elements || elements.lenght > 1) {
+                throw new Error('nope or too much containers # ' + className);
             }
-            : {
-                preset: 'twirl#blackDotIcon',
-            }
-    );
-    map.geoObjects.add(placemark);
 
-    function getContainer(className) {
-        var elements = document.getElementsByClassName(className);
-
-        if (!elements || elements.lenght > 1) {
-            throw 'nope or too much containers # ' + className;
+            return elements[0];
         }
 
-        return elements[0];
-    }
+        function readData(container) {
+            function smartRead(container, attribute, value, fail) {
+                value = value || undefined;
+                fail = fail || false;
 
-    function readData(container) {
-        function smartRead(container, attribute, value, fail) {
-            value = value || undefined;
-            fail = fail || false;
-
-            var data = container.getAttribute(attribute);
-            if (data) {
-                return data;
-            } else if (value) {
-                return value;
-            } else if (!fail) {
-                return data;
-            } else {
-                throw 'broken map data # ' + attribute;
+                var data = container.getAttribute(attribute);
+                if (data) {
+                    return data;
+                } else if (value) {
+                    return value;
+                } else if (!fail) {
+                    return data;
+                } else {
+                    throw new Error('broken map data # ' + attribute);
+                }
             }
-        }
 
-        var positionLatitude = smartRead(container, 'data-place-latitude'),
-            positionLongitude = smartRead(container, 'data-place-longitude');
+            var positionLatitude = smartRead(container, 'data-place-latitude'),
+                positionLongitude = smartRead(container, 'data-place-longitude');
 
-        return {
-            position: [
-                positionLatitude,
-                positionLongitude
-            ],
-            config: {
-                center: [
-                    smartRead(container, 'data-center-latitude', positionLatitude),
-                    smartRead(container, 'data-center-longitude', positionLongitude)
+            return {
+                position: [
+                    positionLatitude,
+                    positionLongitude
                 ],
-                zoom: smartRead(container, 'data-zoom', 15),
-            },
-            icon : smartRead(container, 'data-icon', null, false),
-        };
-    }
-});
+                config: {
+                    center: [
+                        smartRead(container, 'data-center-latitude', positionLatitude),
+                        smartRead(container, 'data-center-longitude', positionLongitude)
+                    ],
+                    zoom: smartRead(container, 'data-zoom', 15),
+                },
+                icon : smartRead(container, 'data-icon', null, false),
+            };
+        }
+    });
+})(ymaps);
